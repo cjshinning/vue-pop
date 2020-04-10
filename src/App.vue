ba3ab837b66f6a9efa38951693ed3f8e9eb96d6c
jie chen <jiechen@192.168.0.101>
2020-04-10 14:31:45 +0800

    普通组件模式

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <input type="button" value="按钮" class="btn" @click="openPop" />
+    <popup text="弹窗内容" ref="popup"></popup>
+  </div>
+</template>
+
+<script>
+import './assets/main.css';
+import Popup from './components/popup/popup';
+export default {
+  name: 'App',
+  methods: {
+    openPop(){
+      console.log(this.$refs.popup);
+      this.$refs.popup.open();
+    }
+  },
+  components: {
+    Popup
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ src/components/popup/popup.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="popup-wrapper" v-show="visible" @click="hide">
+        <div class="popup-text">{{text}}</div>
+    </div>
+</template>
+<script>
+export default {
+    name: 'popup',
+    props: ['text'],
+    data(){
+        return {
+            visible: false
+        }
+    },
+    methods: {
+        hide(){
+            this.visible = false;
+        },
+        open(){
+            this.visible = true;
+        }
+    }
+}
+</script>
+<style>
+.popup-wrapper{
+    position: fixed;
+    width: 100%;
+    height: 100%;
+}
+.popup-text{
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%,-50%);
+    width: 60%;
+    height: 40px;
+    line-height: 40px;
+    background: rgba(0,0,0,0.8);
+    border-radius:5px; 
+    color: #fff;
+    text-align: center;
+}
+</style>
